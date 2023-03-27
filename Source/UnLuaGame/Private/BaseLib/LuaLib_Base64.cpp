ba21a8b00f4908cc818ca4df4fef7bdbc2d214d9
ToAmuse <909185693@qq.com>
2023-03-27 11:11:16 +0800

    添加 LuaLib_Base64 编解码

---
+++ Source/UnLuaGame/Private/BaseLib/LuaLib_Base64.cpp
@@ -0,0 +1,17 @@
+// Copyright 2022 CQUnreal. All Rights Reserved.
+
+#include "UnLuaEx.h"
+#include "LuaCore.h"
+#include "Misc/Base64.h"
+
+static const luaL_Reg FBase64Lib[] =
+{
+	{ nullptr, nullptr }
+};
+
+BEGIN_EXPORT_REFLECTED_CLASS(FBase64)
+	ADD_STATIC_FUNCTION_EX("Encode", FString, Encode, const FString& /*Source*/)
+	ADD_STATIC_FUNCTION_EX("Decode", bool, Decode, const FString& /*Source*/, FString& /*OutDest*/)
+	ADD_LIB(FBase64Lib)
+END_EXPORT_CLASS()
+IMPLEMENT_EXPORTED_CLASS(FBase64)
