f973da8f75ce31c0b751bbdf786c533455328cca
ToAmuse <909185693@qq.com>
2023-03-29 18:43:22 +0800

    UE5.2兼容性修改

--- Source/UnLuaGame/Private/BaseLib/LuaLib_Base64.cpp
+++ Source/UnLuaGame/Private/BaseLib/LuaLib_Base64.cpp
@@ -9,9 +9,21 @@ static const luaL_Reg FBase64Lib[] =
 	{ nullptr, nullptr }
 };
 
+#if ENGINE_MAJOR_VERSION >= 5 && ENGINE_MINOR_VERSION >= 2
+BEGIN_EXPORT_ENUM(EBase64Mode)
+	ADD_SCOPED_ENUM_VALUE(Standard)
+	ADD_SCOPED_ENUM_VALUE(UrlSafe)
+END_EXPORT_ENUM(EBase64Mode)
+#endif
+
 BEGIN_EXPORT_CLASS(FBase64)
+#if ENGINE_MAJOR_VERSION >= 5 && ENGINE_MINOR_VERSION >= 2
+	ADD_STATIC_FUNCTION_EX("Encode", FString, Encode, const FString& /*Source*/, EBase64Mode /*Mode = EBase64Mode::Standard*/)
+	ADD_STATIC_FUNCTION_EX("Decode", bool, Decode, const FString& /*Source*/, FString& /*OutDest*/, EBase64Mode /*Mode = EBase64Mode::Standard*/)
+#else
 	ADD_STATIC_FUNCTION_EX("Encode", FString, Encode, const FString& /*Source*/)
 	ADD_STATIC_FUNCTION_EX("Decode", bool, Decode, const FString& /*Source*/, FString& /*OutDest*/)
+#endif
 	ADD_LIB(FBase64Lib)
 END_EXPORT_CLASS()
 IMPLEMENT_EXPORTED_CLASS(FBase64)
