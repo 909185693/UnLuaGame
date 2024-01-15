8189c1bc05499cabe990a22ce97720fc160343b0
山城老司机 <909185693@qq.com>
2024-01-15 22:55:12 +0800

    删除修改ControllerId代码

--- Source/UnLuaGame/Private/UnLuaGameViewportClient.cpp
+++ Source/UnLuaGame/Private/UnLuaGameViewportClient.cpp
@@ -9,26 +9,3 @@ UUnLuaGameViewportClient::UUnLuaGameViewportClient(const FObjectInitializer& Obj
 {
 
 }
-
-bool UUnLuaGameViewportClient::InputTouch(FViewport* InViewport, int32 ControllerId, uint32 Handle, ETouchType::Type Type, const FVector2D& TouchLocation, float Force, FDateTime DeviceTimestamp, uint32 TouchpadIndex)
-{
-	if (IgnoreInput())
-	{
-		return false;
-	}
-
-	ULocalPlayer* LocalPlayer = GetGameInstance()->GetLocalPlayerByIndex(0);
-
-	// route to subsystems that care
-	bool bResult = (ViewportConsole ? ViewportConsole->InputTouch(LocalPlayer ? LocalPlayer->GetControllerId() : ControllerId, Handle, Type, TouchLocation, Force, DeviceTimestamp, TouchpadIndex) : false);
-	if (!bResult)
-	{
-		ULocalPlayer* const TargetPlayer = LocalPlayer ? LocalPlayer : GEngine->GetLocalPlayerFromControllerId(this, ControllerId);
-		if (TargetPlayer && TargetPlayer->PlayerController)
-		{
-			bResult = TargetPlayer->PlayerController->InputTouch(Handle, Type, TouchLocation, Force, DeviceTimestamp, TouchpadIndex);
-		}
-	}
-
-	return bResult;
-}
--- Source/UnLuaGame/Public/Engine/UnLuaGameViewportClient.h
+++ Source/UnLuaGame/Public/Engine/UnLuaGameViewportClient.h
@@ -13,7 +13,4 @@ UCLASS()
 class UNLUAGAME_API UUnLuaGameViewportClient : public UGameViewportClient
 {
 	GENERATED_UCLASS_BODY()
-
-public:
-	virtual bool InputTouch(FViewport* Viewport, int32 ControllerId, uint32 Handle, ETouchType::Type Type, const FVector2D& TouchLocation, float Force, FDateTime DeviceTimestamp, uint32 TouchpadIndex) override;
 };
