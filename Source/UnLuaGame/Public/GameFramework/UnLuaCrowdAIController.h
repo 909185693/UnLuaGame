f7064e489da2b04abcf0b8d4a20a0af532b0011f
山城老司机 <909185693@qq.com>
2024-03-05 23:14:35 +0800

    添加AUnLuaCrowdAIController

---
+++ Source/UnLuaGame/Public/GameFramework/UnLuaCrowdAIController.h
@@ -0,0 +1,16 @@
+// Copyright 2022 CQUnreal. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "UnLuaCrowdAIController.generated.h"
+
+/**
+ * AUnLuaCrowdAIController
+ */
+UCLASS()
+class UNLUAGAME_API AUnLuaCrowdAIController : public AAIController
+{
+	GENERATED_UCLASS_BODY()
+};
